5b2a047a8c1e086584d535d2f0780f62c2be95b7
Andrew Ashbacher <andrewashbacher@gmail.com>
2019-02-04 12:20:32 -0800

    fix class name in BaseIssue spec

--- spec/lib/rubira/base_issue_spec.rb
+++ spec/lib/rubira/base_issue_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe Rubira::Issue do
+RSpec.describe Rubira::BaseIssue do
   it_behaves_like 'a basic issue'
   it_behaves_like 'an issue with basic associations'
 end
